c3c78bf8bea9200b5236fd5979da797fd1521ddd
Padmanabha8 <49566837+Padmanabha8@users.noreply.github.com>
2021-02-24 08:46:34 +0530

    Update Capstoning.py

--- Capstoning.py
+++ Capstoning.py
@@ -1,4 +1,3 @@
-
 # FFT ALGORITHM
 import cv2
 import numpy as np
@@ -23,7 +22,7 @@ cv2.destroyAllWindows()
 #eye_cascade = cv2.CascadeClassifier('haarcascade_eye_default.xml')
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+#eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 catface_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalcatface.xml')
 cap = cv2.VideoCapture(0)
@@ -41,10 +40,11 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
+    '''
     eyes = eye_cascade.detectMultiScale(roi_gray)    
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color, (ex,ey),(ex+ew,ey+eh), (0,255,0), 2)
-            
+    '''        
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xFF
     if k==27:
